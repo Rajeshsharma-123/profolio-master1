00b4fc1192e3ae42f2c040dac814fdb81383089b
Rajeshsharma-123 <079bct060@ioepc.edu.np>
2025-10-09 20:57:01 +0545

    send api updated

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -40,8 +40,8 @@ export async function POST(req: Request) {
     }
 
     const data = await resend.emails.send({
-      from: 'My Website <no-reply@Rajesh.my>',
-      to: ['contact@Rajesh.my'],
+      from: 'My Website <onboarding@resend.dev>',
+      to: ['raj9807464548@gmail.com'],
       reply_to: email,
       subject: subject,
       react: ContactEmail({ email, name, message })
